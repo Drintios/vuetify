615843b3db298114862abe4059df6d1ad11a7efa
Rafael <rafael.jose1201@gmail.com>
2018-12-29 19:06:53 -0300

    Release for new version of fixed cols and header

---
+++ es5/components/VDataTable/mixins/colgroup.js
@@ -0,0 +1,49 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+/* @vue/component */
+exports.default = {
+    methods: {
+        genTColgroup: function genTColgroup() {
+            if (!this.headers.every(function (header) {
+                return header.width;
+            })) return; // Exit we don't have width specified for all cols
+            var children = this.genCols();
+            return this.$createElement('colgroup', children);
+        },
+        genCols: function genCols() {
+            var columns = [];
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = this.headers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var header = _step.value;
+
+                    columns.push(this.$createElement('col', {
+                        attrs: { width: header.width }
+                    }));
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+
+            return columns;
+        }
+    }
+};
+//# sourceMappingURL=colgroup.js.map
--- packages/vuetifyjs.com/src/plugins/index.js
+++
@@ -1,4 +0,0 @@
-// Make this dynamic
-import '@/plugins/axios'
-import './rollbar'
-import '@/plugins/vuetify'
